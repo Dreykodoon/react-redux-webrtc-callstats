762f88214309d87d036f72e93f7ddacdd09ba67c
mcioban <mihai.cioban@8x8.com>
2020-09-02 16:58:56 +0300

    adding information to store

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { addUser } from "./actions/userActions";
 
 
 function App() {
+    const dispatch = useDispatch();
+
     const users = useSelector(state => state.user.users);
 
-    return (
-    <div>
-      <p>Hello, World!</p>
-      {users}
-    </div>
+    const onClickAddUser = () => {
+      dispatch(addUser('another user'));
+    };
 
-);
+    return (
+        <div>
+          <p>Hello, World!</p>
+          {users}
+          <button onClick={onClickAddUser}>Add user</button>
+        </div>
+    );
 }
 
 export default App;
